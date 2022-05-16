306315260efff94ca820b83337a27de51854dc87
ssallam <36411297+ssallam@users.noreply.github.com>
2022-05-16 10:43:33 +0200

    add lotus scripts

---
+++ bitscreen-scripts/bitscreen.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# setup bitscreen
+# install bitscreen-cli
+# run bitscreen-cli setup install
+# restart lotus miner
+# run storage deal
+# run retrieval deal
+# add cid to filter
+# run retrieval deal
---
+++ lotus-scripts/main-node.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+cd ~/filecoin-work/lotus-local-net
+
+export LOTUS_PATH=~/.lotus-local-net
+export LOTUS_MINER_PATH=~/.lotus-miner-local-net
+export LOTUS_SKIP_GENESIS_CHECK=_yes_
+export CGO_CFLAGS_ALLOW="-D__BLST_PORTABLE__"
+export CGO_CFLAGS="-D__BLST_PORTABLE__"
+
+make 2k
+./lotus fetch-params 2048
+./lotus-seed pre-seal --sector-size 2KiB --num-sectors 2
+./lotus-seed genesis new localnet.json
+./lotus-seed genesis add-miner localnet.json ~/.genesis-sectors/pre-seal-t01000.json
+
+# start the node daemon
+./lotus daemon --lotus-make-genesis=devgen.car --genesis-template=localnet.json --bootstrap=false
