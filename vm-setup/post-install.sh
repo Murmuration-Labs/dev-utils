e01bf5f73f43e2c464399127c0d46430e213e5f6
ssallam <36411297+ssallam@users.noreply.github.com>
2022-05-17 12:03:06 +0200

    skip apt install confirmations

--- vm-setup/post-install.sh
+++ vm-setup/post-install.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
 # setup python
-sudo apt install libzmq3-dev
+sudo apt -y install libzmq3-dev
 
-sudo apt-get install python3-pip
-sudo apt-get install python3.9-dev
-sudo apt autoremove
+sudo apt-get install -y python3-pip
+sudo apt-get install -y python3.9-dev
+sudo apt -y autoremove
 
-sudo apt install python3.9-venv
+sudo apt install -y python3.9-venv
 python3.9 -m pip install --user virtualenv
 
 mkdir filecoin-work
 cd filecoin-work
 
-sudo apt install mesa-opencl-icd ocl-icd-opencl-dev gcc git bzr jq pkg-config curl clang build-essential hwloc libhwloc-dev wget -y && sudo apt upgrade -y
+sudo apt install -y mesa-opencl-icd ocl-icd-opencl-dev gcc git bzr jq pkg-config curl clang build-essential hwloc libhwloc-dev wget -y && sudo apt upgrade -y
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 wget -c https://golang.org/dl/go1.17.9.linux-amd64.tar.gz -O - | sudo tar -xz -C /usr/local
 echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
