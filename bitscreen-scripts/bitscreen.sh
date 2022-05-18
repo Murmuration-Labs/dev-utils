14bb927405e90ee2bfc393f10d9fb7893ea64e37
ssallam <36411297+ssallam@users.noreply.github.com>
2022-05-18 15:12:42 +0200

    skip confirmation when uninstalling typer.

--- bitscreen-scripts/bitscreen.sh
+++ bitscreen-scripts/bitscreen.sh
@@ -14,12 +14,14 @@ python3.9 -m venv venv
 source venv/bin/activate
 
 python3.9 -m pip install bitscreen-cli
-python3.9 -m pip uninstall typer
+python3.9 -m pip uninstall -y typer
 python3.9 -m pip install typer
 
 export LOTUS_PATH=~/.lotusDevnet
 export LOTUS_MINER_PATH=~/.lotusminerDevnet
-bitscreen-cli setup install
+
+#bitscreen-cli auth login
+bitscreen-cli setup install --cli=1 --key=$1
 
 #deactivate
 
--- vm-setup/post-install.sh
+++ vm-setup/post-install.sh
@@ -64,7 +64,7 @@ python3.9 -m venv venv
 source venv/bin/activate
 
 python3.9 -m pip install bitscreen-cli
-python3.9 -m pip uninstall typer
+python3.9 -m pip uninstall -y typer
 python3.9 -m pip install typer
 
 export LOTUS_PATH=~/.lotusDevnet
